f9855545764d737c310b2b3f999735be6eb08ade
Najwa <najwa.satirah@ui.ac.id>
2017-09-23 00:44:03 +0700

    Tutorial 3 done

---
+++ src/main/java/com/example/tutorial3/controller/StudentController.java
@@ -0,0 +1,88 @@
+package com.example.tutorial3.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import com.example.tutorial3.service.InMemoryStudentService;
+import com.example.tutorial3.service.StudentService;
+import com.example.tutorial3.model.StudentModel;
+
+@Controller
+public class StudentController {
+	private final StudentService studentService;
+	
+	public StudentController() {
+		studentService = new InMemoryStudentService();
+	}
+	
+	@RequestMapping("/student/add")
+	public String add(@RequestParam(value="npm", required = true) String npm, 
+			@RequestParam(value="name", required = true) String name, 
+			@RequestParam(value="gpa", required = true) double gpa ) {
+		
+		StudentModel student = new StudentModel(npm, name, gpa);
+		studentService.addStudent(student);
+		return "add";
+	}
+	
+//	@RequestMapping("/student/view")
+//	public String view(Model model, @RequestParam(value="npm", required= true) String npm) {
+//		StudentModel student = studentService.selectStudent(npm);
+//		model.addAttribute("student", student);
+//		return "view";
+//	}
+	
+	@RequestMapping("/student/viewall")
+	public String viewall(Model model) {
+		List<StudentModel> students = studentService.selectAllStudents();
+		model.addAttribute("students", students);
+		return "viewall";
+	}
+	
+	@RequestMapping(value = { "/student/view", "student/view/{npm}" })
+	public String viewpath(@PathVariable Optional<String> npm, Model model) {
+		if (npm.isPresent()) {
+			StudentModel student = studentService.selectStudent(npm.get());
+			if (student == null) {
+				String error = "Tidak ada NPM tersebut";
+				model.addAttribute("error", error);
+				return "error";
+			} else {
+				model.addAttribute("student", student);
+				return "view";
+			}
+		} else {
+			String error = "Silahkan masukkan NPM";
+			model.addAttribute("error", error);
+			return "error";
+		}
+	}
+	
+	@RequestMapping(value = { "/student/delete", "student/delete/{npm}" })
+	public String deletepath(@PathVariable Optional<String> npm, Model model) {
+		if (npm.isPresent()) {
+			StudentModel student = studentService.selectStudent(npm.get());
+			if (student == null) {
+				String error = "Tidak ada NPM tersebut. Delete dibatalkan.";
+				model.addAttribute("error", error);
+				return "error";
+			} else {
+				if (studentService.deleteStudent(npm.get())) {
+					model.addAttribute("status", "Student berhasil dihapus");
+					return "delete";
+				}
+			}
+		} else {
+			String error = "Silahkan masukkan NPM. Delete dibatalkan.";
+			model.addAttribute("error", error);
+			return "error";
+		}
+		return "error";
+	}
+	
+}
